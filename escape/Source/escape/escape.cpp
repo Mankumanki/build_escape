a03fcd30f505607f6c5b87e302612c21f5d417df
sarthakmehta20100@gmail.com <sarthakmehta20100@gmail.com>
2021-02-10 23:08:46 +0530

    first commit

---
+++ escape/Source/escape/escape.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "escape.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, escape, "escape" );
